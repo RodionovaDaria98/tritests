e1ec347a69084e17455bc762563daed4480b8906
Rodionova Daria <32214402+RodionovaDaria98@users.noreply.github.com>
2020-09-29 14:54:44 +0300

    ch
    ch

--- triangle/UnitTestProject1/UnitTest1.cs
+++ triangle/UnitTestProject1/UnitTest1.cs
@@ -12,7 +12,27 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
-            Triangle t = new Triangle(5, 7, 9);
+
+
+            Assert.AreEqual(TriangleType.Error,
+                            Triangle.GettypofTriangle(4, 0, 4),
+                            "GetTriangleType(4, 0, 4) did not return Error");
+
+            Assert.AreEqual(TriangleType.Error,
+                            Triangle.GettypofTriangle(1, 4, 2),
+                            "GetTriangleType(1, 4, 2) did not return Error");
+
+            Assert.AreEqual(TriangleType.RightTriangle,
+                            Triangle.GettypofTriangle(1, 1, Math.Sqrt(2)),
+                            "GetTriangleType(1, 1, Math.Sqrt(2)) did not return RightTriangle");
+
+            Assert.AreEqual(TriangleType.OxygonTriangle,
+                            Triangle.GettypofTriangle(4, 1, 4),
+                            "GetTriangleType(4, 1, 4) did not return OxygonTriangle");
+
+            Assert.AreEqual(TriangleType.ObtuseTriangle,
+                            Triangle.GettypofTriangle(655, 650, 50),
+                            "GetTriangleType(655, 650, 50) did not return ObtuseTriangle");
         }
     }
 }
--- triangle/trianglesides/Class1.cs
+++ triangle/trianglesides/Class1.cs
@@ -1,72 +1,59 @@
 ﻿using System;
 
 namespace triangleslides
+
 {
-    public class Triangle 
+
+
+
+    public enum TriangleType
     {
-        private readonly int a;
-        private readonly int b;
-        private readonly int c;
-        public Triangle(int a, int b, int c)
-        {
-            this.a = a;
-            this.b = b;
-            this.c = c;
+        RightTriangle = 1,  // Прямоугольный
+        OxygonTriangle = 2, //Остроугольный
+        ObtuseTriangle = 3, //Тупоугольный
+        Error = 4
+    }
 
-            Init();
-        }
+    public class Triangle
+    {
+        public static TriangleType GettypofTriangle(double a, double b, double c)
 
-        public int A
         {
-            get { return this.a; }
-        }
-        public int B
-        {
-            get
-            {
-                return this.b;
-            }
-        }
-        public int C
-        {
-            get
-            {
-                return this.c;
-            }
-        }
-       
+          
 
-       
-        private bool Init()
-        {
             if (a <= 0 || b <= 0 || c <= 0)
-                throw new ArgumentException("Длины сторон треугольника должны быть больше нуля!!!");
+                return TriangleType.Error;
+
 
             if (a > b + c || b > a + c || c > a + b)
-                throw new ArgumentException("Не корректно указаны строны треугольника. Ни одна из  сторон треугольника не должна превосходить сумму других строн!!!");
-            if((a * a + b * b <= c * c) || (a * a + c * c <= b * b) || (c * c + b * b <= a * a))
+                return TriangleType.Error;
+
+            if ((a * a + b * b <= c * c) || (a * a + c * c <= b * b) || (c * c + b * b <= a * a))
             {
                 if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (c * c + b * b == a * a))
                 {
-                    Console.WriteLine("прямоугольный");
+
+
+                    return TriangleType.RightTriangle;
                 }
                 else
                 {
-                    Console.WriteLine("остроугольный");
+                    return TriangleType.OxygonTriangle;
 
                 }
 
             }
             else
             {
-                Console.WriteLine(" тупоугольный");
+                return TriangleType.ObtuseTriangle;
 
             }
-            return true;
 
-           
+
         }
-     
+
+
+
     }
 }
 
